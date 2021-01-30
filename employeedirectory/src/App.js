705336862447edc3fa0006db07f4420a45e828d7
kirbycreese <70101203+kirbycreese@users.noreply.github.com>
2021-01-29 22:26:47 -0500

    created mockup of employee table

--- employeedirectory/src/App.js
+++ employeedirectory/src/App.js
@@ -1,11 +1,13 @@
 import Header from './components/Header/Header'
 import Subheader from './components/Subheader/Subheader.js'
+import EmployeeTable from './components/EmployeeTable/EmployeeTable.js'
 
 function App() {
   return (
     <div className='container'>
      <Header />
      <Subheader />
+     <EmployeeTable />
     </div>
   );
 }
---
+++ employeedirectory/src/components/EmployeeTable/EmployeeTable.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+function EmployeeTable() {
+  return (
+      <div className="EmployeeTable">
+        <div class="container-fluid">
+
+            <ul class="list-group list-group-horizontal-xxl">
+                <li class="list-group-item">
+                    Employee Name
+                </li>
+
+                <li class="list-group-item">
+                    Employee Name
+                </li>
+
+                <li class="list-group-item">
+                    Employee Name
+                </li>
+
+
+
+            </ul>
+
+        </div>
+      </div>
+    
+  );
+}
+
+export default EmployeeTable; 
