eb3f38fabd0b60dc2a1cf710160c06f9861db80b
kirbycreese <70101203+kirbycreese@users.noreply.github.com>
2021-02-10 09:45:50 -0500

    created table for employee data

--- src/components/EmployeeTable/EmployeeTable.js
+++ src/components/EmployeeTable/EmployeeTable.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react";
 import API from "../../utils/Api.js";
+import { Table } from "react-bootstrap";
 
 function EmployeeTable() {
   //create state
   const [data, setData] = useState([]);
-
+  console.log(data);
   useEffect(() => {
     API.getEmployeeData()
       .then((res) => setData(res.data.results))
@@ -14,7 +15,18 @@ function EmployeeTable() {
   return (
     <div>
       {data.map((employee) => (
-        <p>{employee.name.first}</p>
+        <p>
+          <Table striped bordered hover variant="dark">
+            <tbody>
+              <tr>
+                <td> {employee.name.first}</td>
+                <td> {employee.name.last}</td>
+                <td>{employee.email}</td>
+                <td>{employee.gender}</td>
+              </tr>
+            </tbody>
+          </Table>
+        </p>
       ))}
     </div>
   );
